a0f2cfa479d8d7f7b8380bcc7b887febc6510bd8
ohayonb <ohayonzzb@gmail.com>
2022-09-27 15:45:55 +0300

    production changes

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
   // 'dbURL': 'mongodb://localhost:27017',
-  'dbURL': `mongodb+srv://frelloADMIN:frelloADMIN@frellodb.iypqt4w.mongodb.net/?retryWrites=true&w=majority`
+  dbURL: `mongodb+srv://frelloADMIN:frelloADMIN@frellodb.iypqt4w.mongodb.net/?retryWrites=true&w=majority`,
 }
